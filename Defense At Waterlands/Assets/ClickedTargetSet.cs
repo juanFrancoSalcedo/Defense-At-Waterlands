7f0c6885890b6fbddd29a94267de36ee96c19fee
Sebastián Franco <regsebas@gmail.com>
2023-07-09 15:38:56 -0500

    [Fix] solve conflicts

--- Defense At Waterlands/Assets/ClickedTargetSet.cs
+++ Defense At Waterlands/Assets/ClickedTargetSet.cs
@@ -1,3 +1,4 @@
+using DAW.Gameplay;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Defense At Waterlands/Assets/NavigationAgent.cs
+++ Defense At Waterlands/Assets/NavigationAgent.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-<<<<<<< HEAD
 namespace DAW.Gameplay
 {
     [RequireComponent(typeof(NavMeshAgent))]
@@ -17,28 +16,3 @@ namespace DAW.Gameplay
         }
     }
 }
-=======
-
-[RequireComponent(typeof(NavMeshAgent))]
-public class NavigationAgent : MonoBehaviour
-{
-    [SerializeField] private Transform target = null;
-
-    //TODO Inject this
-    NavMeshAgent agentComponent => GetComponent<NavMeshAgent>();
-    
-    private void Awake()
-    {
-        
-    }
-
-    private void Update()
-    {
-    }
-
-    public void Put() 
-    {
-        agentComponent.SetDestination(target.position);
-    }
-}
->>>>>>> 6eda13344b3ad50d7674cede8ee115287a2ce545
